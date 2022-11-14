3d58988cf574bffd0a170a46d46b410272ec434f
Gulnara Laksh <g.talkanbaeva@gmail.com>
2022-11-15 02:35:40 +0600

    task 2.1 edited

--- task_2/solution.js
+++ task_2/solution.js
@@ -7,11 +7,9 @@ function calcShipping(sum, min, shipping) {
 
     let shippingSum; // создайте переменную shippingSum
 
-    if (productsSum == 0) { // если productsSum равно 0,
+    if (productsSum == 0 || productsSum >= freeShippingMinSum)  { // если productsSum равно 0,
     shippingSum = 0; // то shippingSum присвоить значение 0
-    } else if (productsSum >= freeShippingMinSum) { // если prosuctsSum больше или равно freeShippingMinSum,
-    shippingSum = 0; // то shippingSum присвоить значение 0
-    } else ((productsSum > 0) && (productsSum < freeShippingMinSum)); { // если productsSum больше 0 и меньше freeShippingMinSum,
+    } else { // если productsSum больше 0 и меньше freeShippingMinSum,
     shippingSum = shippingPrice; // то shippingSum присвоить значение shippingPrice
     } 
     
