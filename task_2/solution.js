c8f7a8af2c36a943ff3377251f224f051c9fe494
Gulnara Laksh <g.talkanbaeva@gmail.com>
2022-11-15 01:37:34 +0600

    task 2.1 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -5,18 +5,18 @@ function calcShipping(sum, min, shipping) {
 
     // Задание №2.1. Рассчитать доставку
 
-    // создайте переменную shippingSum
+    let shippingSum; // создайте переменную shippingSum
 
-    // если productsSum равно 0,
-    // то shippingSum присвоить значение 0
+    if (productsSum = 0) { // если productsSum равно 0,
+    shippingSum = 0; // то shippingSum присвоить значение 0
+    } else if (productsSum >= freeShippingMinSum) { // если prosuctsSum больше или равно freeShippingMinSum,
+    shippingSum = 0; // то shippingSum присвоить значение 0
+    } else ((productsSum > 0) && (productsSum < freeShippingMinSum)); { // если productsSum больше 0 и меньше freeShippingMinSum,
+    shippingSum = shippingPrice; // то shippingSum присвоить значение shippingPrice
+    } 
+    
 
-    // если productsSum Больше или равна freeShippingMinSum,
-    // то shippingSum присвоить значение 0
-
-    // если productsSum больше 0 и меньше freeShippingMinSum,
-    // то shippingSum присвоить значение shippingPrice
-
-    // Конец решения задания №2.1.
+    // Конец решения задания №21.1.
 
     return shippingSum;
 }
