177a7705b72ec6c3328957ae4cc497fbe9459deb
Gulnara Laksh <g.talkanbaeva@gmail.com>
2022-11-23 01:25:19 +0600

    task 5.1 done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,10 +34,10 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {client: {name, phone}, order: {address, sum}, goods: [{title, count}]};
+    let data = {client: {name, phone}, order: {address, sum}, goods: []};
     let countOfGoods = goods.length;
     for (let i = 0; i < countOfGoods; i += 1) {
-        data.goods.push({title: goods.title[i], count: goods.count[i]});
+        data.goods.push({title: goods[i].title, count: goods[i].count});
     }
 
     data.goods = (goods.title, goods.count);
